e8d3a31aa4ce5ead5152f672776891564031634b
Akash-Edake <akashedake30@gmail.com>
2024-03-26 12:06:29 +0530

    NEXTAUTH_URL

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,11 +6,12 @@ import { useSession, signIn, signOut } from "next-auth/react"
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const { data: session } = useSession()
+  const { data: session,accessToken } = useSession()
   if (session) {
     return (
       <>
         Signed in as {session.user.email} <br />
+        Access Token: {session.accessToken} <br />
         <button onClick={() => signOut()}>Sign out</button>
       </>
     )
